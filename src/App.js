4de4744c38cae77584f29ba533c3935a5a62a46a
Kazi Rahman <kazirahm@gmail.com>
2020-03-16 22:38:22 -0700

    refactoring a bit

--- src/App.js
+++ src/App.js
@@ -23,61 +23,70 @@ class App extends React.Component {
 
   responseGoogle = (response) => {
     console.log("yeee")
-    this.setState({ userDetails: response.profileObj, isSignedIn: true });
+    this.setState({userDetails: response.profileObj, isSignedIn: true });
   };
 
   logout = () => {
     this.setState({isSignedIn: false, userDetails:{}})
   };
 
+  checkSignedIn = () => {
+    console.log("we in here", this.state.isSignedIn)
+    if(!this.state.isSignedIn){
+      return(
+        <GoogleLogin
+        clientId="1034011358451-901ird0jj5jnle85inpimfpba5epnaqv.apps.googleusercontent.com"
+        render={renderProps => (
+          <button
+            className="button"
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+          >
+            Log in with Google
+          </button>
+        )}
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+      />
+      )
+    }else{
+      console.log("we returning else")
+      return(
+        <div className="userDetails-wrapper">
+        <div className="details-wrapper">
+          <GoogleLogout
+            render={renderProps => (
+              <button
+                className="logout-button"
+                onClick={renderProps.onClick}
+              >
+                Log Out
+              </button>
+            )}
+            onLogoutSuccess={this.logout}
+          />
+
+          <div className="image">
+            <img src={this.state.userDetails.imageUrl} />
+          </div>
+          <div className="name">
+            Welcome {this.state.userDetails.givenName}{" "}
+            {this.state.userDetails.familyName}
+          </div>
+          <div className="email"><i>{this.state.userDetails.email}</i></div>
+        </div>
+        <div className="bar" />
+        <div className="stand" />
+      </div>
+      )
+    }
+  }
+
   
   render(){
     return(
       <div className="App">
-        {!this.state.isSignedIn && (
-          <GoogleLogin
-            clientId="1034011358451-901ird0jj5jnle85inpimfpba5epnaqv.apps.googleusercontent.com"
-            render={renderProps => (
-              <button
-                className="button"
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-              >
-                Log in with Google
-              </button>
-            )}
-            onSuccess={this.responseGoogle}
-            onFailure={this.responseGoogle}
-          />
-        )}
-        {this.state.isSignedIn && (
-          <div className="userDetails-wrapper">
-            <div className="details-wrapper">
-              <GoogleLogout
-                render={renderProps => (
-                  <button
-                    className="logout-button"
-                    onClick={renderProps.onClick}
-                  >
-                    Log Out
-                  </button>
-                )}
-                onLogoutSuccess={this.logout}
-              />
-
-              <div className="image">
-                <img src={this.state.userDetails.imageUrl} />
-              </div>
-              <div className="name">
-                Welcome Mr. {this.state.userDetails.givenName}{" "}
-                {this.state.userDetails.familyName}
-              </div>
-              <div className="email"><i>{this.state.userDetails.email}</i></div>
-            </div>
-            <div className="bar" />
-            <div className="stand" />
-          </div>
-        )}
+        {this.checkSignedIn()}
       </div>
     );
   }
